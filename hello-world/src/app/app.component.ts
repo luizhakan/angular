2064aeb39c0496135a3c06611ceb8e9d2bda4f14
luizhakan <luizhakan2@protonmail.com>
2023-02-14 12:52:46 -0300

    compartilhamento de dados em angular

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName: string = 'luizhakan';
+  userData = {
+    email: 'luizhakan2@protonmail.com',
+    role: 'Admin',
+  };
   title = 'hello-world';
 }
