db6dc68246504dcf6715289bace5342c29a328ad
luizhakan <luizhakan2@protonmail.com>
2023-02-14 11:51:58 -0300

    interpolacao de dados

--- hello-world/src/app/components/first-component/first-component.component.ts
+++ hello-world/src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent {
-
+  name: string = 'Luiz Hakan';
+  age: number = 20;
+  job: string = 'Developer';
+  hobbies: string[] = ['Programar', ' Ver minha namorada'];
+  car = {
+    name: 'Fiat Toro',
+    year: 2019,
+  };
 }
