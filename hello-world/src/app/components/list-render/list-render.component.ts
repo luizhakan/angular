c2f111aa68ae01ac5efad77d55f17df5ee8e70ff
luizhakan <luizhakan2@protonmail.com>
2023-02-14 15:19:06 -0300

    interfaces em angular

--- hello-world/src/app/components/list-render/list-render.component.ts
+++ hello-world/src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Animal } from '../Animal';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals: { name: string; type: string }[] = [
-    { name: 'Lyon', type: 'Lion' },
-    { name: 'Cheetos', type: 'Tiger' },
-    { name: 'Dumble', type: 'Elephant' },
-    { name: 'Random', type: 'Giraffe' },
+  animals: Animal[] = [
+    { name: 'Lyon', type: 'Lion', age: 5 },
+    { name: 'Cheetos', type: 'Tiger', age: 3 },
+    { name: 'Dumble', type: 'Elephant', age: 1 },
+    { name: 'Random', type: 'Giraffe', age: 2 },
   ];
+
+  animalDetails: string = '';
+
+  showAge(animal: Animal): string {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`;
+    return this.animalDetails;
+  }
 }
