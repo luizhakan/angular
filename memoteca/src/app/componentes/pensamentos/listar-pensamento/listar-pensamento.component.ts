96a27546931732b0dfb1c3318120b0079442412d
luizhakan <luizhakan2@protonmail.com>
2023-08-11 12:05:34 -0300

    Sec 4
    Nesta seção eu pratiquei os tipos de diretivas, esta é uma descrição:
    
    Diretivas de componentes: usado com um modelo. Esse tipo de diretiva é a mais comum.
    
    Ex: <app-listarPensamentos>.
    
    Diretivas estruturais: altera o layout do DOM adicionando e removendo elementos DOM.
    
    Ex: NgIf, NgFor. NgSwitch.
    
    Diretivas de atributos: altera a aparência ou o comportamento de um elemento, componente ou outra diretiva.
    
    Ex: NgClass, NgStyle.

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'I love Angular',
+      autoria: 'Luiz Hakan',
+      modelo: 'modelo3',
+    },{
+      conteudo: 'I love Java',
+      autoria: 'Luiz Hakan',
+      modelo: 'modelo1',
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dignissimos vitae earum quasi aspernatur voluptatum necessitatibus sequi aut temporibus, maxime praesentium possimus in repellat harum provident tempore voluptates quod ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dignissimos vitae earum quasi aspernatur voluptatum necessitatibus sequi aut temporibus, maxime praesentium possimus in repellat harum provident tempore voluptates quod ipsam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dignissimos vitae earum quasi aspernatur voluptatum necessitatibus sequi aut temporibus, maxime praesentium possimus in repellat harum provident tempore voluptates quod ipsam',
+      autoria: 'Luiz Hakan',
+      modelo: 'modelo2',
+    }
+  ];
+
   constructor() {}
 
   // nessa função, eu quero que crie um link para a rota criarPensamento
