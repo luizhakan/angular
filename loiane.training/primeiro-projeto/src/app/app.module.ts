3b2c4a9564a1ca13833e303b11b4a0bd35a2841b
luizhakan <luizhakan2@protonmail.com>
2023-09-07 18:58:39 -0300

    Introdução
    Nesta seção eu pratiquei criação de componente fora da cli, módulos
    (ngModule), templates, serviços e injeção de dependência

--- loiane.training/primeiro-projeto/src/app/app.module.ts
+++ loiane.training/primeiro-projeto/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component],
+  imports: [BrowserModule, AppRoutingModule, CursosModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
